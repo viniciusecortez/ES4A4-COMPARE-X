550008ab3c6eabe9ef5cbf5e2014aef020272002
Vinicius Eustaquio Cortez <viniciusecortez@outlook.com>
2019-10-19 23:05:30 -0300

    Colocado spring pelo spring initializer

---
+++ src/main/java/br/edu/ifsp/lp2a2/comparex/comparex/ComparexApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.lp2a2.comparex.comparex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComparexApplication.class, args);
+	}
+
+}
