3c84849bd87122b4d7f4bd681d486f09c90e456d
Vinicius Eustaquio Cortez <viniciusecortez@outlook.com>
2019-10-26 03:20:05 -0200

    Inicializado controller

--- src/main/java/br/edu/ifsp/lp2a2/comparex/comparex/comum/controllers/HomeController.java
+++ src/main/java/br/edu/ifsp/lp2a2/comparex/comparex/comum/controllers/HomeController.java
@@ -3,12 +3,18 @@ package br.edu.ifsp.lp2a2.comparex.comparex.comum.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 class HomeController{
    @GetMapping("/")
    public String index(Model model){
         return "comum/index";
+   }
+   @PostMapping("/search")
+   public String search(Model model){
+       return null;
+       
    }
 }
