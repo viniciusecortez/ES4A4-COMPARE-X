c1d847e7126b38cc71ac91c5d1b1a36b5057f675
Vinicius Eustaquio Cortez <viniciusecortez@outlook.com>
2019-11-01 11:11:23 -0300

    Adicionadas classes de repositório e model de Produto

--- src/main/java/br/edu/ifsp/lp2a2/comparex/comparex/comum/model/entidades/ProdutosRespository.java
+++ src/main/java/br/edu/ifsp/lp2a2/comparex/comparex/comum/model/entidades/ProdutosRespository.java
@@ -1,4 +1,8 @@
 package br.edu.ifsp.lp2a2.comparex.comparex.comum.model.entidades;
 
-public class ProdutosRespository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutosRespository extends CrudRepository<Produto, Long> {
 }
