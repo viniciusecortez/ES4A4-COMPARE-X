d62d226d941cddc3386a8d73df67cb6ecd632855
Vinicius Eustaquio Cortez <42585259+viniciusecortez@users.noreply.github.com>
2019-11-07 02:35:32 -0300

    Update HomeController.java

--- src/main/java/br/edu/ifsp/lp2a2/comparex/comum/controllers/HomeController.java
+++ src/main/java/br/edu/ifsp/lp2a2/comparex/comum/controllers/HomeController.java
@@ -13,7 +13,7 @@ import br.edu.ifsp.lp2a2.comparex.comum.model.entidades.ProdutosRespository;
 @Controller
 public class HomeController{
     @GetMapping("/")
-    public String list(ProdutosRespository repository, Model model){
+    public String list(){
         return "comum/index";
     }
    @GetMapping(value = "/search", params = {"pesquisar"})
