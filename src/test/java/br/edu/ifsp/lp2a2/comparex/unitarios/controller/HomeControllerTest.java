0d9ba228037864020b109cfdfd0de32b1e734a26
Vinicius Eustaquio Cortez <42585259+viniciusecortez@users.noreply.github.com>
2019-11-07 02:30:57 -0300

    Update HomeControllerTest.java

--- src/test/java/br/edu/ifsp/lp2a2/comparex/unitarios/controller/HomeControllerTest.java
+++ src/test/java/br/edu/ifsp/lp2a2/comparex/unitarios/controller/HomeControllerTest.java
@@ -3,6 +3,8 @@ package br.edu.ifsp.lp2a2.comparex.unitarios.controller;
 import br.edu.ifsp.lp2a2.comparex.comum.controllers.HomeController;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.springframework.ui.Model;
+import br.edu.ifsp.lp2a2.comparex.comum.model.entidades.ProdutosRespository;
 
 import org.junit.Test;
 
@@ -12,7 +14,7 @@ public class HomeControllerTest {
     public void index_return_template() {
         // Configuracao
         HomeController controller = new HomeController();
-        String actual = controller.index();
+        String actual = controller.index(new ProdutosRespository(), new Model());
         String expected = "comum/index";
         assertEquals(expected, actual);
     }
